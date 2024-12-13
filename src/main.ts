12b047a1d6b4a8049a4ec61c3428f7a349f8b93d
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2024-12-13 16:15:21 +0100

    feat: user can show todos with enter and button
    closes #5

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,34 @@
 import './style.css'
 
 console.log('Hello from typescript')
+const input = document.querySelector<HTMLInputElement>('#todo-input')
+const output = document.querySelector('.todo-element')
+const button = document.querySelector('#add-todo-button')
+
+function addtolist(): void {
+  if (input && output) {
+    const text: string = input.value.trim()
+    if (text) {
+      const li = document.createElement('li')
+      li.textContent = text
+      output.appendChild(li)
+      input.value = ''
+    }
+  } else {
+    throw new Error('refresh page web')
+  }
+}
+if (input && button) {
+
+  input.addEventListener('keydown', (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      addtolist()
+    }
+  })
+  button.addEventListener("click", () => {
+    addtolist()
+  })
+
+}else {
+  throw new Error('refresh page web')
+}
