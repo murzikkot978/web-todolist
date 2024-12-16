3ca1e43bae72d55213bcfbc28b473d5c61384d34
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2024-12-16 11:21:07 +0100

    feat: user can store todos to local storage
    close #4

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,26 @@ console.log('Hello from typescript')
 const input = document.querySelector<HTMLInputElement>('#todo-input')
 const output = document.querySelector('.todo-element')
 const button = document.querySelector('#add-todo-button')
+const todos: string[] = []
 
 function addtolist(): void {
   if (input && output) {
     const text: string = input.value.trim()
     if (text) {
       const li = document.createElement('li')
+
+      const myObj_deserialized = localStorage.getItem('todo_list')
+      todos.push(text.toString())
+
+      const myObj_serialized = JSON.stringify(todos)
+      localStorage.setItem('todo_list', myObj_serialized)
+
       li.textContent = text
       output.appendChild(li)
       input.value = ''
+
+      console.log(localStorage)
+      console.log(myObj_deserialized)
     }
   } else {
     throw new Error('refresh page web')
